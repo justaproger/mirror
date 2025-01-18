1e04939ec4b67cc3ba0c690cdf760891d7f21dac
unknown <justaproger@gmail.com>
2025-01-18 22:58:16 +0500

    Fix for Railway deployment

--- my_mirror_site/settings.py
+++ my_mirror_site/settings.py
@@ -149,4 +149,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-ALLOWED_HOSTS = ['localhost', 'mirror-production-c9d7.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','mirror-production-c9d7.up.railway.app', '[::1]']
