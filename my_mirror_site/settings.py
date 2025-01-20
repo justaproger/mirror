d6f7d3e549e983e086ce2d927104fe3aa9076448
unknown <justaproger@gmail.com>
2025-01-20 14:01:17 +0500

    transfering to aws

--- my_mirror_site/settings.py
+++ my_mirror_site/settings.py
@@ -16,7 +16,7 @@ import corsheaders
 from django.conf.global_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
