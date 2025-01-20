6477c09b1b74776b908f67f2a62b35e8902bff6a
unknown <justaproger@gmail.com>
2025-01-20 13:57:47 +0500

    transfering to aws

--- my_mirror_site/settings/prod.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-DEBUG = False
-
-ALLOWED_HOSTS = ['tender-ambition-production.up.railway.app','flask-production-42ee8.up.railway.app','*']
-
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'https://tender-ambition-production.up.railway.app').split(',')
